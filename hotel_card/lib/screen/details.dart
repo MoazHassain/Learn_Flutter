9f4f10a8ddcbcd5dbd94cee8802407c061b85cb3
MoazHassain <moazhassain@gmail.com>
2022-07-12 19:21:22 +0600

    structured details page

--- hotel_card/lib/screen/details.dart
+++ hotel_card/lib/screen/details.dart
@@ -60,77 +60,188 @@ class _DetailsState extends State<Details> {
       ),
       body: Container(
         width: double.infinity,
-        child: Column(
+        child: Stack(
           children: [
-            Container(
-              child: Row(
-                children: [
-                  Text(
-                    "Hotels For You",
+            Column(
+              children: [
+                Container(
+                  child: Row(
+                    children: [
+                      Text(
+                        "Hotels For You",
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.edit,
+                      ),
+                      Icon(
+                        Icons.search,
+                      ),
+                    ],
                   ),
-                  Spacer(),
-                  Icon(
-                    Icons.edit,
+                ),
+                Text(
+                  "150 results",
+                ),
+                Container(
+                  height: 60,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        // margin: EdgeInsets.only(
+                        //   right: 20,
+                        // ),
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return SizedBox(
+                        height: 20,
+                        width: 20,
+                      );
+                    },
                   ),
-                  Icon(
-                    Icons.search,
+                ),
+                Container(
+                  height: 4,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.blue[600],
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                ],
-              ),
-            ),
-            Text(
-              "150 results",
-            ),
-            Container(
-              height: 60,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                shrinkWrap: true,
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.only(
-                      right: 20,
-                    ),
-                    height: 60,
-                    width: 60,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.red,
-                    ),
-                  );
-                },
-              ),
-            ),
-            Container(
-              height: 4,
-              width: 50,
-              decoration: BoxDecoration(
-                color: Colors.blue[600],
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-            Container(
-              child: Card(
-                child: Column(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.27,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            "",
+                ),
+                Container(
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.27,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                "",
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "\$600.00",
+                                    ),
+                                    Text(
+                                      "Booking ID: 9837432",
+                                    ),
+                                    Text(
+                                      "Wickwicky Hotel",
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "6.0",
+                                    ),
+                                    Text(
+                                      "Rating",
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Text(
+                        "Facilities",
+                      ),
+                      Spacer(),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text("SEE MORE"),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  child: ListView.separated(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 4,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              child: Icon(
+                                Icons.wifi,
+                              ),
+                            ),
+                            Text("WIFI"),
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return SizedBox(
+                        width: 20,
+                        height: 20,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 15,
+              right: 60,
+              left: 60,
+              child: Container(
+                color: Colors.blue,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
                     Container(
-                      child: Row(
+                      width: double.infinity,
+                      height: 60,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Container(
-                            child: Column(),
+                          Text(
+                            "Booking Now",
                           ),
                         ],
+                      ),
+                    ),
+                    Positioned(
+                      left: 20,
+                      child: Icon(
+                        Icons.check_circle,
                       ),
                     ),
                   ],
